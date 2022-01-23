341a488abff7421711188133b1fe2664bfbb5bf7
ooAkira <lee.quockhanh1107@gmail.com>
2022-01-23 22:55:52 +0700

    done code tinh_tong_4.cpp

---
+++ Thuật toán/FullHouse/TINH_TONG_4.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, s = 0;
+    do
+    {
+        cin >> n;
+    } while (n <= 0);
+
+    for (int i = 1; i <= n; i++)
+    {
+        s += i * 2;
+    }
+
+    cout << s;
+
+    return 0;
+}
