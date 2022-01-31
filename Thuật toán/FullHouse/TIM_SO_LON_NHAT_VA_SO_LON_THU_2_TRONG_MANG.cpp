badd0a31a7cd43f2aee249e19e5e4f7b77f13e29
ooAkira <lee.quockhanh1107@gmail.com>
2022-01-31 23:33:58 +0700

    Clean code

--- Thuật toán/FullHouse/TIM_SO_LON_NHAT_VA_SO_LON_THU_2_TRONG_MANG.cpp
+++ Thuật toán/FullHouse/TIM_SO_LON_NHAT_VA_SO_LON_THU_2_TRONG_MANG.cpp
@@ -8,21 +8,15 @@ int main()
     cin >> n;
     int a[n];
     for (int i = 0; i < n; i++)
-    {
         cin >> a[i];
-    }
     for (int i = 0; i < n; i++)
-    {
         for (int j = i + 1; j < n; j++)
-        {
             if (a[i] < a[j])
             {
                 int bien_phu = a[i];
                 a[i] = a[j];
                 a[j] = bien_phu;
             }
-        }
-    }
     cout << a[0] << endl;
     cout << a[1];
 }
