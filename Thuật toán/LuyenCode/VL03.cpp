30c44a79df153c7bcf88167876d19c84f35cbcf4
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-06 11:53:53 +0700

    Done testcase 4/4 VL03

---
+++ Thuật toán/LuyenCode/VL03.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, s = 0;
+    do
+    {
+        cin >> n;
+    } while (n >= pow(10, 4));
+    for (int i = 2; i <= n; i++)
+    {
+        s += i;
+    }
+    cout << s + 2 * n;
+}
