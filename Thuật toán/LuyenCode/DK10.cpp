de256dce3461d12b99539f85a07d2f4fbf53cb70
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-06 11:42:31 +0700

    Testcase DK10: 4/5

--- Thuật toán/LuyenCode/DK10.cpp
+++ Thuật toán/LuyenCode/DK10.cpp
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int kiemtranamnhuan(int year)
+{
+    if (year % 4 == 0 && year % 100 != 0)
+        return 1;
+    else
+        return 0;
+}
 int main()
 {
     int month, year;
-    do
-    {
-        cin >> month >> year;
-    } while (0 > month >= 100 && 0 > year >= 100000);
-
-    if (1 <= month <= 12 && 0 < year <= 100000)
+    cin >> month >> year;
+    if ((month >= 1 || month <= 12) && (year > 0 || year <= 100000))
     {
         switch (month)
         {
@@ -27,6 +31,15 @@ int main()
         case 11:
             cout << "30";
             break;
+        case 2:
+            if (kiemtranamnhuan(year) == 1)
+            {
+                cout << "29";
+            }
+            else
+            {
+                cout << "28";
+            }
         }
     }
     else
