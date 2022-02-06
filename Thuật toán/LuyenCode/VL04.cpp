93d984f7121d5786987cd9cbc8daf8669cc22015
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-06 12:01:10 +0700

    Testcase VL04: 4/4

--- Thuật toán/LuyenCode/VL04.cpp
+++ Thuật toán/LuyenCode/VL04.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main()
 {
-    int n, sum = 0;
+    int n;
+    float sum = 0;
     do
     {
         cin >> n;
     } while (n <= 2 && n >= pow(10, 4));
-    for (int i = 2; i <= n; i++)
+    for (float i = 2; i <= n; i++)
         sum += 1 / i;
+    cout << setprecision(4) << fixed << sum;
 }
