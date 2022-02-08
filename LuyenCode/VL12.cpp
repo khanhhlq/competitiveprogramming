e05f716628cd1b3cfae89352779c14923c842ec0
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-08 21:56:54 +0700

    AC 5/5

--- LuyenCode/VL12.cpp
+++ LuyenCode/VL12.cpp
@@ -7,7 +7,13 @@ int main()
     {
         cin >> n;
     } while (abs(n) >= pow(10, 4));
-    for (int i = n; i >= 1; i--)
-        if (n % i == 0)
-            cout << i << " ";
+    n = abs(n);
+    if (n == 0)
+        cout
+            << "INF";
+    else
+        for (int i = n; i >= 1; i--)
+            if (n % i == 0)
+                cout
+                    << i << " ";
 }
