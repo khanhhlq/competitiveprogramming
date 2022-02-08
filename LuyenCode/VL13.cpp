b3a2fe969fe215ceffaf41c5b03ecd3d5fd63a38
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-08 22:01:56 +0700

    Testcase VL13: 3/5

--- LuyenCode/VL13.cpp
+++ LuyenCode/VL13.cpp
@@ -2,13 +2,20 @@
 using namespace std;
 int main()
 {
-    int n, i = 1;
+    int n, i = 1, result;
     do
     {
         cin >> n;
     } while (abs(n) >= pow(10, 9));
 
-    for (int i = 1; i <= n; i +=)
+    n = abs(n);
+    for (int i = n; i >= 1; i--)
     {
+        if (n % i == 0)
+            result += i;
     }
+    if (result == n)
+        cout << "YES";
+    else
+        cout << "NO";
 }
