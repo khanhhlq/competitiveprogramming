8acf76707bfd90de0b066924a2d5b095d70f332c
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-08 21:21:12 +0700

    AC VL10

--- Thuật toán/LuyenCode/VL10.cpp
+++ Thuật toán/LuyenCode/VL10.cpp
@@ -1,1 +1,11 @@
-#include <bits/c
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    if (s[0] == '-')
+        cout << s.length() - 1;
+    else
+        cout << s.length();
+}
