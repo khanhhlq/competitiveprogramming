4fbb217dd218d33e12e50e943757b10db8036781
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-08 22:07:25 +0700

    AC  VL13

--- LuyenCode/VL13.cpp
+++ LuyenCode/VL13.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 int main()
 {
-    int n, i = 1, result;
+    int n, i = 1, result = 0;
     do
     {
         cin >> n;
     } while (abs(n) >= pow(10, 9));
 
-    n = abs(n);
-    if (n == 0)
-        cout << "INF";
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+            result += i;
+    }
+    if (result == n)
+        cout << "YES";
     else
-        for (int i = n; i >= 1; i--)
-            if (n % i == 0)
-                cout
-                    << i << " ";
+        cout << "NO";
 }
