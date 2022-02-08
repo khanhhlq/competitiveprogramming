5a9fe07b7c4f508eb8e2f3b9d9d0e8564d663346
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-08 21:59:17 +0700

    Create files VL13.cpp

---
+++ LuyenCode/VL13.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, i = 1;
+    do
+    {
+        cin >> n;
+    } while (abs(n) >= pow(10, 9));
+
+    for (int i = 1; i <= n; i +=)
+    {
+    }
+}
