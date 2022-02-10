1b3741145deabc77ec27ca3c6cbfc37543113b25
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-10 22:10:53 +0700

    AC VL19

--- LuyenCode/VL19.cpp
+++ LuyenCode/VL19.cpp
@@ -1,23 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-void xuat(int a, int b)
+int main()
 {
-    int dem = 0;
-    for (int i = b; i > a; i--)
+    int a, b, dem = 0;
+    do
     {
+        cin >> a >> b;
+    } while (abs(a) >= 10000 && abs(b) >= 10000 && a <= b);
+
+    for (int i = b; i > a; i--)
         if (i % 3 == 0 && (i > a && i < b))
         {
             cout << i << " ";
             dem++;
         }
-    }
     if (dem == 0)
         cout << "NOT FOUND";
-}
-int main()
-{
-    int a, b;
-    cin >> a >> b;
-    xuat(a, b);
-    return 0;
 }
