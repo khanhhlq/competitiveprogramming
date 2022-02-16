a610be900376c0903dacebabec6da2faa6de0868
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-16 22:46:17 +0700

    AC 100%

--- LuyenCode/VT03.cpp
+++ LuyenCode/VT03.cpp
@@ -1,12 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int n;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    
-    sort(arr + 1, arr + 1 + n);
-    cout << arr[0];
+    int max = arr[0], cs;
+    for (int i = 0; i < n; i++)
+    {
+        if (max <= arr[i])
+        {
+            max = arr[i];
+            cs = i;
+        }
+    }
+
+    cout << cs;
 }
