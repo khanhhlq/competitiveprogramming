6eae52707d2ebe53db470e7c38ed0e45980ae9d6
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-16 22:38:12 +0700

    Not AC: 1/4

---
+++ LuyenCode/VT03.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    sort(arr + 1, arr + 1 + n);
+    cout << arr[0];
+}
