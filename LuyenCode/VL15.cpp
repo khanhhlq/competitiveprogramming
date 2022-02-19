7967bc70b021597c662ba96559013729ff9884c4
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-19 15:38:51 +0700

    AC VL15

---
+++ LuyenCode/VL15.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    do
+    {
+        cin >> a >> b;
+    } while (abs(a) >= 1000 && abs(b) >= 1000);
+
+    if (b == 0)
+        cout << "INVALID" << endl;
+    else
+    {
+        if (a % b == 0)
+            cout << a / b;
+        else
+        {
+            int new_a = a;
+            int new_b = b;
+            int tmp = a;
+            while (b != 0)
+            {
+                tmp = a % b;
+                a = b;
+                b = tmp;
+            }
+            cout << new_a / a << " " << new_b / a;
+        }
+    }
+}
