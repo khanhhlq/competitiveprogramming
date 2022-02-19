cf3d8b658dcd5d2256990a3fab1a9299cbd9c6ca
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-19 13:33:53 +0700

    AC VL14

--- LuyenCode/VL14.cpp
+++ LuyenCode/VL14.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int uocchung(int a, int b)
+
+int ucln(int a, int b)
 {
-    if (a > b)
-        a = a - b;
-    else if (b > a)
-        b = b - a;
-    else if (a == b)
-        return a;
+    if (a == 0 || b == 0)
+        return a + b;
+    while (a != b)
+    {
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    return a;
 }
 
 int main()
@@ -18,5 +23,5 @@ int main()
         cin >> a >> b;
     } while (abs(a) >= 1000 && abs(b) >= 1000);
 
-    cout << uocchung(abs(a), abs(b));
+    cout << ucln(abs(a), abs(b));
 }
