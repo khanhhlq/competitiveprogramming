49898a52f7151918f5ef0c88e7f7bdb35bdce444
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-22 21:39:43 +0700

    AC VT06 4/4

--- VT06.cpp
+++ VT06.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 int main()
 {
-    long long n;
+    int n;
     do
     {
         cin >> n;
     } while (n >= 1 && n >= pow(10, 3));
-    long long arr[n];
+    int arr[n];
     for (int i = 0; i < n; i++)
         do
         {
             cin >> arr[i];
         } while (arr[i] >= pow(10, 6));
-    int TBC = 0;
+    int TBC = 0, dem = 0;
     for (int i = 0; i < n; i++)
-        if (arr)
-    float result = TBC / n;
-    cout << fixed << setprecision(4) << result;
+        if (arr[i] % 2 != 0)
+        {
+            TBC += arr[i];
+            dem++;
+        }
+    cout << fixed << setprecision(4) << (float)TBC / dem;
 }
