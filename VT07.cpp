07d3a8705e7d370ddf66c5f5d435c698128385d8
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-22 22:01:13 +0700

    AC VT07.cpp

--- VT07.cpp
+++ VT07.cpp
@@ -3,7 +3,25 @@ using namespace std;
 int main()
 {
     int arr[11];
-    for (int i = 0; i < 11; i++)
+    for (int i = 0; i < 10; i++)
     {
+        do
+        {
+            cin >> arr[i];
+        } while (abs(arr[i] >= pow(10, 9)));
+    }
+    int x, dem = 0;
+    cin >> x;
+    for (int i = 0; i < 10; i++)
+    {
+        if (x == arr[i])
+        {
+            cout << i + 1 << ' ';
+            dem++;
+        }
+    }
+    if (dem == 0)
+    {
+        cout << "-1";
     }
 }
