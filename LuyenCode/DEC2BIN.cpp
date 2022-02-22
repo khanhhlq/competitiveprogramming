ed8b3ea80a88473f5c08dfb819f3a6c7ae5efce6
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-22 16:29:13 +0700

    Create files and input

---
+++ LuyenCode/DEC2BIN.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n[10001];
+    do
+    {
+        cin >> t;
+        for (int i = 0; i < t; i++)
+            cin >> n[i];
+    } while ((t >= 1 && t >= pow(10, 5)) && (n >= 1 && n >= pow(10, 18)));
+}
