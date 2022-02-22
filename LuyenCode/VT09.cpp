3b4e9e9c4f7c6863c3090ac8038ba0221eb747c3
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-22 23:14:14 +0700

    Fixbug

--- LuyenCode/VT09.cpp
+++ LuyenCode/VT09.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int prime(int n)
+{
+    if (n < 2)
+    {
+        return 0;
+    }
+    else
+    {
+        for (int i = 2; i <= sqrt(n); i++)
+            return 2;
+    }
+
+    return 1;
+}
 int main()
 {
     int n;
@@ -13,7 +28,9 @@ int main()
         {
             cin >> arr[i];
         } while (arr[i] >= pow(10, 8));
+    sort(arr[n], arr[n] + 1 + n);
     for (int i = 0; i < n; i++)
-        if(arr[i] % )
-    
+    {
+        cout << prime(arr[i]) << " ";
+    }
 }
