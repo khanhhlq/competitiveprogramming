6b03d59e1261123ecb5a9f88df6108de89291b99
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-22 16:40:52 +0700

    Fixbugs: DEC TO BIN

--- LuyenCode/DEC2BIN.cpp
+++ LuyenCode/DEC2BIN.cpp
@@ -1,14 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int DEC2BIN(int n)
+{
+    if (n != 0)
+    {
+        DEC2BIN(n / 2);
+        cout << n % 2;
+    }
+}
+
 int main()
 {
 
-    int t, n[10001];
+    int t, n[1000000];
     do
     {
         cin >> t;
-        for (int i = 0; i < t; i++)
-            cin >> n[i];
-    } while ((t >= 1 && t >= pow(10, 5)) && (n >= 1 && n >= pow(10, 18)));
+
+    } while (t >= 1 && t >= pow(10, 5));
+
+    for (int i = 0; i < t; i++)
+        cin >> n[i];
+
+    for (int i = 0; i < t; i++)
+    {
+        cout << DEC2BIN(n[i]);
+    }
 }
