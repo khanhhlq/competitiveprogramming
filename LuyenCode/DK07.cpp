b74632d65d01f5ba050f3cdf8eb5b85da1f01287
ooAkira <lee.quockhanh1107@gmail.com>
2022-03-09 21:58:00 +0700

    AC 10/10

--- LuyenCode/DK07.cpp
+++ LuyenCode/DK07.cpp
@@ -1,40 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int a, b, c, denta;
-    float x1, x2;
-    do
-    {
-        cin >> a >> b >> c;
-    } while (-1000 >= a, b, c >= 1000);
-    denta = b * b - 4 * a * c;
-    if (a == 0)
-    {
-        if (b == 0)
-        {
-            if (c == 0)
-                cout << "INF";
-            else
-                cout << "NO";
-        }
-        else if (b != 0)
-            cout << setprecision(2) << fixed << (float)(-c) / b;
-    }
-    else
-    {
-        if (denta < 0)
-            cout << "NO";
-        else if (denta == 0)
-            cout << setprecision(2) << fixed << (float)-b / (2 * a);
-        else
-        {
-            x1 = (float)(-sqrt(denta) - b) / (2 * a);
-            x2 = (float)(sqrt(denta) - b) / (2 * a);
-            if (x1 > x2)
-                cout << setprecision(2) << fixed << x2 << " " << x1;
-            else
-                cout << setprecision(2) << fixed << x1 << " " << x2;
-        }
-    }
+int main() {
+	int a, b, c, denta;
+	float x1, x2;
+	do {
+		cin >> a >> b >> c;
+	} while (-1000 >= a, b, c >= 1000);
+	denta = b * b - 4 * a * c;
+	if (a == 0) {
+		if (b == 0) {
+			if (c == 0)
+				cout << "INF";
+			else
+				cout << "NO";
+		} else if (b != 0)
+			cout << setprecision(2) << fixed << (float)(-c) / b;
+	} else {
+		if (denta < 0)
+			cout << "NO";
+		else if (denta == 0)
+			cout << setprecision(2) << fixed << (float)-b / (2 * a);
+		else {
+			x1 = (float)(-sqrt(denta) - b) / (2 * a);
+			x2 = (float)(sqrt(denta) - b) / (2 * a);
+			if (x1 > x2)
+				cout << setprecision(2) << fixed << x2 << " " << x1;
+			else
+				cout << setprecision(2) << fixed << x1 << " " << x2;
+		}
+	}
 }
