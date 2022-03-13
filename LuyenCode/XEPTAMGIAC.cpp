acc82453054a4da42dd93e0b7814a9b96ceac690
ooAkira <lee.quockhanh1107@gmail.com>
2022-03-13 13:43:06 +0700

    Clean code

--- LuyenCode/XEPTAMGIAC.cpp
+++ LuyenCode/XEPTAMGIAC.cpp
@@ -4,19 +4,10 @@ int main()
 {
     int l[10000], dem = 0;
     for (int i = 0; i < 5; i++)
-    {
         cin >> l[i];
-    }
-
     for (int i = 0; i < 5; i++)
-    {
         for (int j = i + 1; j < 5; j++)
-        {
             if (l[i] + l[j] > l[j + 2])
-            {
                 dem++;
-            }
-        }
-    }
     cout << dem;
 }
