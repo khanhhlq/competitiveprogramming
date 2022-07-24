4579f97d42170b083c94be47260a412e5d0a0ec8
YWtpcmE <86475463+ooAkira@users.noreply.github.com>
2022-07-24 22:59:37 +0700

    Fit(*): Clean code

--- FullHouse/DEM_SO_NGUYEN_TO.cpp
+++ FullHouse/DEM_SO_NGUYEN_TO.cpp
@@ -6,27 +6,33 @@ void getData(int a[], int &n){
         cin >> n;
     } while (n <= 1 && n >= 10000);
     
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++){
         cin >> a[i];
+    }
 }
 
 int checkData(int k){
-    if (k < 2)
-        return 0; 
+    if (k < 2){
+        return 0;
+    }
     else{
-        for (int i = 2; i <= k/2; i++)
-            if (k % i == 0)
+        for (int i = 2; i <= k/2; i++){
+            if (k % i == 0){
                 return 0;
+            }
+        }
         return 1;
     }
-
 }
 
 void printData(int a[], int &n){
     int count = 0;
-    for (int i = 0; i < n; i++)
-        if (checkData(a[i]))
+    for (int i = 0; i < n; i++){
+        if (checkData(a[i])){
             count++;
+        }   
+    }
+   
     cout << count;        
 }
 
