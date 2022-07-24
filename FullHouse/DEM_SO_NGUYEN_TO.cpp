0a47a146ba3dd1fe8036b7cf7df86b16bd99f393
YWtpcmE <86475463+ooAkira@users.noreply.github.com>
2022-07-24 23:04:54 +0700

    Finish DEM_SO_NGUYEN_TO

--- FullHouse/DEM_SO_NGUYEN_TO.cpp
+++ FullHouse/DEM_SO_NGUYEN_TO.cpp
@@ -1,45 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void getData(int a[], int &n){
-    do{
-        cin >> n;
-    } while (n <= 1 && n >= 10000);
-    
-    for (int i = 0; i < n; i++){
-        cin >> a[i];
-    }
-}
-
 int checkData(int k){
-    if (k < 2){
+    if (k < 2)
         return 0;
-    }
     else{
-        for (int i = 2; i <= k/2; i++){
-            if (k % i == 0){
+        for (int i = 2; i <= k/2; i++)
+            if (k % i == 0)
                 return 0;
-            }
-        }
         return 1;
     }
 }
 
-void printData(int a[], int &n){
-    int count = 0;
-    for (int i = 0; i < n; i++){
-        if (checkData(a[i])){
-            count++;
-        }   
-    }
-   
-    cout << count;        
-}
-
 int main(){
     int n;
-    int a[1000];
-    getData(a, n);
-    printData(a, n);
-    return 0;
+    int a[100];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int count = 0;
+    for (int i = 0; i < n; i++)
+        if (checkData(a[i]))
+            count++;
+    cout << count;
 }
