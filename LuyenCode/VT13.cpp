a9e0347075a8eed1dad4827d14a45e13adcd387f
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-13 00:00:13 +0700

    VT13-fixing

--- LuyenCode/VT13.cpp
+++ LuyenCode/VT13.cpp
@@ -1,29 +1,28 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 int main()
 {
     int n;
     cin >> n;
     int a[n];
+
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    int max = 0, sum = 0, b[3];
-    for (int i = 0; i < n + 1; i++)
-    {
+
+    int max = a[0] + a[n - 1];
+    cout << max;
+    int sum = 0; 
+    int b[3];
+
+    for (int i = 0; i <= n - 2; i++){
         sum = a[i] + a[i + 1];
-        if (sum >= max)
-        {
+
+        if(sum > max){
             max = sum;
             b[0] = a[i];
             b[1] = a[i + 1];
+            cout << b[0] << " " << b[1];
         }
-    }
-    if (b[0] > b[1])
-    {
-        cout << b[0] << " " << b[1];
-    }
-    else
-    {
-        cout << b[1] << " " << b[0];
-    }
+    }   
 }
