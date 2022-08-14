4649a994cf7b320f144be28c0905719990e54c43
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-14 19:11:53 +0700

    clear - khong_uoc.cpp

--- Exercise/KHONG_UOC.cpp
+++ Exercise/KHONG_UOC.cpp
@@ -1,20 +1,16 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    long long n, ans = 1, dem = 0, i = 2;
-    cin >> n;
-
-    while(i <= n){
-        if(n % i ==0){
+    long long n,ans=1,dem=0,i=2;
+    cin>>n;
+    
+    while(i<=n){
+        if(n%i==0){
             dem++;
             long long demphu=0;
-            while(n % i == 0){
-                demphu++;
-                n/=i;
-            }
-            ans*=(demphu+ 1);
+            while(n%i==0){demphu++; n/=i;}
+            ans*=(demphu+1);
         }
         i++;
     }
