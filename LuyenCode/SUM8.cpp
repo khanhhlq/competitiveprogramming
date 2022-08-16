5ae99d85a13ebf8eb13f9b785b5092e63dc5ae14
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-16 23:37:43 +0700

    AC _ SUM8

---
+++ LuyenCode/SUM8.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int T, n, ans=0; cin>>T;
+    while(T--){
+        cin>>n;
+        for (int i=n; i>=1; i--){
+            ans = sqrt(i)
+        }
+    }
+}
