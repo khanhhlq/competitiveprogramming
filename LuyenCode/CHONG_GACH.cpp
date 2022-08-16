9d504d94d18f62f770b1c653eefb0578a65e3917
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-17 00:49:31 +0700

    AC DK01 & DK02

--- LuyenCode/CHONG_GACH.cpp
+++ LuyenCode/CHONG_GACH.cpp
@@ -5,44 +5,11 @@ int main()
     int n;
     cin >> n;
     int a[n];
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> a[i];
-    }
+    for (int i = 0; i < n; ++i){cin >> a[i];}
     sort(a, a + n, greater<int>());
     int idx = min(n - 1, a[0]);
     int i = 1;
-    while (i < idx)
-    {
-        idx = min(idx, i + a[i]);
-        i++;
-    }
+    while (i < idx){idx = min(idx, i + a[i]); i++;}
     cout << idx + 1;
     return 0;
 }
-
-// Cách giải 1
-
-// #include <iostream>
-// #include <bits/stdc++.h>
-
-//     using namespace std;
-// int main()
-// {
-//     int n, k = 0, a[101];
-//     cin >> n;
-//     for (int i = 1; i <= n; i++)
-//         cin >> a[i];
-//     sort(a + 1, a + n + 1);
-//     for (int i = 1; i <= n; i++)
-//         if (a[i] >= k)
-//         {
-//             k++;
-//             for (int i = 1; i <= n; i++)
-//             {
-//                 a[i] = arr[i] - 1;
-//             }
-//         };
-//     cout << k;
-//     return 0;
-// }
