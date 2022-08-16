82f54b80239091184411fb07e5091ab6fba60632
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-16 22:22:14 +0700

    AC - NAME.cpp

---
+++ Exercise/NAME.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin,s);
+    int i = s.length();
+    int a=s.find("  ");
+    while(a>-1 && i >= 1){s.erase(a,1); a=s.find("  ");}
+    while(i--){
+        if(s[i]!=' '&&s[i-1]==' '){s[i]=toupper(s[i]);}
+        else{s[i]=tolower(s[i]);}
+    }
+    s[0] = toupper(s[0]);
+    s.erase(s.begin() +  0);
+    cout << s << endl;
+}
