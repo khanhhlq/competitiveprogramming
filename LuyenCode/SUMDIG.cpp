c4a05d3bc3ee721351399a2a377e0341984ba7c4
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-19 02:47:31 +0700

    AC SUMDIG

--- LuyenCode/SUMDIG.cpp
+++ LuyenCode/SUMDIG.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main(){
-    int n,ans; cin>>n;
+    int n,ans=0; cin>>n;
     string s;
     while(n--){
         ans=0;
         cin>>s;
-        for(int i=0; i<s.size(); i++){ans+=(int)s[i]-48;}
-        cout << ans << endl;
+        for(int i=0; i<s.size(); i++) ans+=(int)s[i]-48;
+        cout<<ans<<endl;
     }
 }
