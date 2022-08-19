613c43cf04998b46251e02b3d0f8de43b88ed249
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-19 22:43:10 +0700

    DEMTU - AC

--- LuyenCode/DEMTU.cpp
+++ LuyenCode/DEMTU.cpp
@@ -1,19 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int dem=0; string s[100002];
-    getline(cin,s);
-    for(int i=0; i<s.length(); i++){
-        if(s[i]==' ' && s[i+1]!=' '){dem++;
-        } else if(s[i]!=' '){
-            dem++;
-        } else{
-            cout << dem;
-        }
-    }
-    cout << dem;
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    string s;
+    getline(cin, s);
+    int x=0; 
+    for(int i=0; i<s.size()-1; i++) if(s[i]==' '&&s[i+1]!=' ') x++;
+    if(s[0]!=' ') cout<<x+1;
+    else cout<<x;
 }
-
-// Python
-// s = input()
-// print(len(s.split()))
