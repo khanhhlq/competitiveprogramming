3eac83611c2fa144fa9250c6ce985d743e084012
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-22 23:48:34 +0700

    AC VT03

--- LuyenCode/VT03.cpp
+++ LuyenCode/VT03.cpp
@@ -1,21 +1,36 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    int max = arr[0], cs;
-    for (int i = 0; i < n; i++)
-    {
-        if (max <= arr[i])
-        {
-            max = arr[i];
-            cs = i;
+int main(){
+    int n, m, k; cin>>n; int a[n];
+    for (int i=0; i<n; i++){
+        cin>>a[i];
+        if(i!=0){
+            m=max(a[i],a[i-1]);
+            if(m==a[i]) k=i;
+            else k=i-1;
         }
     }
-
-    cout << cs;
+    cout<<k;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//         cin >> arr[i];
+//     int max = arr[0], cs;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (max <= arr[i])
+//         {
+//             max = arr[i];
+//             cs = i;
+//         }
+//     }
+
+//     cout << cs;
+// }
