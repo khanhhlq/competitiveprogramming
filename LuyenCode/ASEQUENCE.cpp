b88170fe9905024fe0f9c4ea0a3a769329e8ad93
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-24 00:12:23 +0700

    AC - AXBXC

---
+++ LuyenCode/ASEQUENCE.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin>>n;
+    vector<int> T;
+    while(n--){
+        getline(cin, T[n])
+    }
+    for(int i=0; i<T.size(); i++){
+        cout << T[n] << " ";
+    }
+}
---
+++ LuyenCode/AXBXC.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main(){
+    ll k; cin>>k;
+    for(int i=1; i<k; i++){
+        
+    }
+}
