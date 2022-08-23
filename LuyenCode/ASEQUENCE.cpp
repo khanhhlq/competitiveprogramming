1857c5855e7d788de8550f0e3303b052e7ea4e22
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-24 00:58:14 +0700

    AC ASEQUENCE

--- LuyenCode/ASEQUENCE.cpp
+++ LuyenCode/ASEQUENCE.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
+typedef long long ll;
 using namespace std;
 int main(){
-    int n; cin>>n;
-    vector<int> T;
-    while(n--){
-        getline(cin, T[n])
-    }
-    for(int i=0; i<T.size(); i++){
-        cout << T[n] << " ";
-    }
+    ll n; cin>>n;
+    ll dem=1, x=n;
+    vector<ll> T;
+    while(n--){ ll k; cin>>k; T.push_back(k); }
+    ll ans=T[1]-T[0];
+    for(ll i=1; i<T.size(); i++)
+        if(T[i+1]-T[i]==ans) dem++;
+    if(dem==(x-1)) cout << "YES";
+    else cout << "NO";
 }
