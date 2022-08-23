032656c2ece778faa384e3c8422eb5b44fdebae2
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-23 22:27:36 +0700

    AC ABC042_A

---
+++ LuyenCode/ABC023_A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=0,n=3;
+    while(n--){
+        int n; cin>>n;
+        if(n==5) a++;
+    }
+    if(a==2) cout<<"YES";
+    else cout << "NO";
+}
