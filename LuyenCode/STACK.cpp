09e694438304a5de455e85f1d71745df83c8051f
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-27 22:00:55 +0700

    AC STACK #2

--- LuyenCode/STACK.cpp
+++ LuyenCode/STACK.cpp
@@ -1,23 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-int t, x, y, top=0, sta[100005];
+stack<int> T;
+int t, x, y;
 int main(){
     cin>>t;
-    for(int i=1; i<=t; i++){
+    cin.ignore();
+    for(int i=0; i<t; i++){
         cin>>x;
         switch(x){
             case 1:
                 cin>>y;
-                top++;
-                sta[top]=y;
+                T.push(y);
                 break;
             case 2:
-                if(top>0) top--;
+                if(!T.empty()) T.pop();
+                else break;
                 break;
             case 3:
-                if(top>0) cout<<sta[top]<<"\n";
+                if(!T.empty()) cout<<T.top()<<"\n";
                 else cout<<"Empty!"<<"\n";
                 break;
         }
     }
 }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int t, x, y, top=0, sta[100005];
+// int main(){
+//     cin>>t;
+//     for(int i=0; i<t; i++){
+//         cin>>x;
+//         switch(x){
+//             case 1:
+//                 cin>>y;
+//                 top++;
+//                 sta[top]=y;
+//                 break;
+//             case 2:
+//                 if(top>0) top--;
+//                 break;
+//             case 3:
+//                 if(top>0) cout<<sta[top]<<"\n";
+//                 else cout<<"Empty!"<<"\n";
+//                 break;
+//         }
+//     }
+// }
