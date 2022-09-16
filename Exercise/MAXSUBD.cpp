decb70b7b5e432fc9d50560613ddb1975100495b
ooAkira <lee.quockhanh1107@gmail.com>
2022-09-16 23:55:27 +0700

    clean code + old code

--- Exercise/MAXSUBD.cpp
+++ Exercise/MAXSUBD.cpp
@@ -2,23 +2,38 @@
 using namespace std;
 typedef long long ll;
 int main(){
-    ll n, d, sum=0, max=0; cin>>n>>d;
+    ll n, d, max=0; cin>>n>>d;
     ll a[n];
-    for(int i=0; i<n; i++){
-        cin>>a[i];
-    }
+    for(int i=0; i<n; i++) cin>>a[i];
     for(int i=0; i<=n-d; i++){
         ll sum_extra=0;
-        for(int j=i; j<i+d; j++){
-            sum_extra+=a[j];
-        }
-        if(sum_extra>max){
-            max=sum_extra;
-        }
-        
-   
+        for(int j=i; j<i+d; j++) sum_extra+=a[j];
+        if(sum_extra>=max) max=sum_extra;
     }
     cout << max;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// typedef long long ll;
+// int main(){
+//     ll n, d, sum=0, max=0; cin>>n>>d;
+//     ll a[n];
+//     for(int i=0; i<n; i++){
+//         cin>>a[i];
+//     }
+//     for(int i=0; i<=n-d; i++){
+//         ll sum_extra=0;
+//         for(int j=i; j<i+d; j++){
+//             sum_extra+=a[j];
+//         }
+//         if(sum_extra>max){
+//             max=sum_extra;
+//         }
+        
+   
+//     }
+//     cout << max;
     
 
-}
+// }
