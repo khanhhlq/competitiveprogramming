b99fc3876f7a46ace89cdd39206f49563cf85c95
ooAkira <lee.quockhanh1107@gmail.com>
2022-09-16 23:51:53 +0700

    done

--- Exercise/MAXSUBD.cpp
+++ Exercise/MAXSUBD.cpp
@@ -2,12 +2,23 @@
 using namespace std;
 typedef long long ll;
 int main(){
-    ll n, d, maxx=0; cin>>n>>d;
+    ll n, d, sum=0, max=0; cin>>n>>d;
     ll a[n];
     for(int i=0; i<n; i++){
-        for(int j=0; j<=d; j++){
-            if(a[i]+a)
-        }
+        cin>>a[i];
     }
+    for(int i=0; i<=n-d; i++){
+        ll sum_extra=0;
+        for(int j=i; j<i+d; j++){
+            sum_extra+=a[j];
+        }
+        if(sum_extra>max){
+            max=sum_extra;
+        }
+        
+   
+    }
+    cout << max;
+    
 
 }
