4e62919327995b1ffeef875bfa1fbc20f5e5c0a6
ooAkira <lee.quockhanh1107@gmail.com>
2022-09-16 23:11:18 +0700

    NOT DONE

---
+++ Exercise/MAXSUBD.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll n, d, maxx=0; cin>>n>>d;
+    ll a[n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=d; j++){
+            if(a[i]+a)
+        }
+    }
+
+}
