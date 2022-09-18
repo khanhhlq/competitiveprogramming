c11e7431d0e5b0fa13967b8caa1ed3b68e316fb4
ooAkira <lee.quockhanh1107@gmail.com>
2022-09-18 20:59:59 +0700

    xam vai on _ lvc

---
+++ bin/18092022.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+string question, answer; 
+int main(){
+    question="Ad thay sao ve em?";
+    cout << question << endl;
+    cin >> answer;
+    cout << "Weoo, ad thay: " << answer << endl;
+    cout << "Eo oi, Iu admin, chuc ad ngu ngon uwu";
+}
